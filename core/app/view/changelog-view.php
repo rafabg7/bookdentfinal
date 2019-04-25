ad387c03d78adb1fcbcd63b01b8f27c070a200c8
rafabg7 <rafa_bg@outlook.com>
2019-04-24 18:17:04 -0600

    first commit

---
+++ core/app/view/changelog-view.php
@@ -0,0 +1,7 @@
+<div class="row">
+<div class="col-md-12">
+<h1>Sistema de Citas Medicas</h1>
+<p>Bienvenido a <b>BookDent</b> un Sistema de Citas Medicas util para consultorios medicos y/o medicos independientes.</p>
+
+</div>
+</div>
---
+++ core/controller/Module.php
@@ -0,0 +1,23 @@
+<?php
+
+
+
+// Module.php
+//  tareas que se realizan con modulos.
+
+class Module {
+
+	public static function loadLayout(){
+		if(Core::$root==""){
+		include "core/app/layouts/layout.php";
+		}else if(Core::$root=="admin/"){
+		include "core/app/".Core::$theme."/layouts/layout.php";
+		}
+	}
+
+
+}
+
+
+
+?>
